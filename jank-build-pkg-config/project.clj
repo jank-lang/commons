07bf033d6b65bec393282049562ceb481b483cec
jeaye <contact@jeaye.com>
2026-06-18 15:51:01 -0700

    Add jank-build-pkg-config

---
+++ jank-build-pkg-config/project.clj
@@ -0,0 +1,6 @@
+(defproject org.jank-lang.commons/jank-build-pkg-config "2026.06-1"
+  :description "Build script helper for querying libraries via pkg-config."
+  :url "https://github.com/jank-lang/commons"
+  :license {:name "MPL 2.0"
+            :url  "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :source-paths ["src"])
