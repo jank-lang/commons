184cdabeb7c79ee549f042ca669a54353599dbaa
Kyle Cesare <kcesare@gmail.com>
2026-06-19 13:00:02 -0600

    imgui-sys: replace custom Makefile with CMake

--- imgui-sys/project.clj
+++ imgui-sys/project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/jank-lang/commons"
   :license {:name "MPL 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.06-1"]]
   :dependencies []
   :plugins [[org.jank-lang/lein-jank "0.7"]]
   :middleware [leiningen.jank/middleware]
-  :verbatim-paths ["lib/imgui" "Makefile"])
+  :verbatim-paths ["lib/imgui" "CMakeLists.txt"])
