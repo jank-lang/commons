47e7d455919e929903cbe678561232cb38359119
jeaye <contact@jeaye.com>
2026-06-25 18:19:42 -0700

    Release imgui-glfw-sys imgui-sys imgui-opengl2-sys raylib-sys jank-build-cmake
    imgui-glfw-sys :: 2026.06-4 -> 2026.06-5
    imgui-sys :: 2026.06-4 -> 2026.06-5
    imgui-opengl2-sys :: 2026.06-4 -> 2026.06-5
    raylib-sys :: 2026.06-5 -> 2026.06-6
    jank-build-cmake :: 2026.06-4 -> 2026.06-5

--- imgui-sys/project.clj
+++ imgui-sys/project.clj
@@ -1,9 +1,9 @@
-(defproject org.jank-lang.commons/imgui-sys "2026.06-4"
+(defproject org.jank-lang.commons/imgui-sys "2026.06-5"
   :description "Raw package for Dear ImGUI."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MPL 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.06-4"]]
+  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.06-5"]]
   :dependencies []
   :plugins [[org.jank-lang/lein-jank "0.7"]]
   :middleware [leiningen.jank/middleware]
