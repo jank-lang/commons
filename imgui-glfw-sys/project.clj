d85c4fc1557dc76d135fb304729f61236fcc7909
jeaye <contact@jeaye.com>
2026-07-21 17:09:04 -0700

    Release imgui-glfw-sys 2026.07-1

--- imgui-glfw-sys/project.clj
+++ imgui-glfw-sys/project.clj
@@ -1,9 +1,9 @@
-(defproject org.jank-lang.commons/imgui-glfw-sys "2026.06-6"
+(defproject org.jank-lang.commons/imgui-glfw-sys "2026.07-1"
   :description "Raw package for Dear ImGUI GLFW platform backend."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MPL 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.06-6"]]
+  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.07-1"]]
   :dependencies [[org.jank-lang.commons/glfw-sys "2026.06-1"]
                  [org.jank-lang.commons/imgui-sys "2026.06-6"]]
   :plugins [[org.jank-lang/lein-jank "0.7"]]
