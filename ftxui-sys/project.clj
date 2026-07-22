294b15b33ac655c679068bfc682ce5feb42f38d8
jeaye <contact@jeaye.com>
2026-07-21 17:09:17 -0700

    Release ftxui-sys 2026.07-1

--- ftxui-sys/project.clj
+++ ftxui-sys/project.clj
@@ -1,9 +1,9 @@
-(defproject org.jank-lang.commons/ftxui-sys "2026.06-1"
+(defproject org.jank-lang.commons/ftxui-sys "2026.07-1"
   :description "Raw package for FTXUI."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MPL 2.0"
             :url  "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.06-2"]]
+  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.07-1"]]
   :plugins [[org.jank-lang/lein-jank "2026.06-1"]]
   :middleware [leiningen.jank/middleware]
   :verbatim-paths ["lib/ftxui"])
