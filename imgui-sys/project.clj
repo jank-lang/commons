936fb4223086cae46e97e70e8072bb5d7f786821
jeaye <contact@jeaye.com>
2026-08-18 15:38:49 -0700

    Release imgui-sys 2026.08-1

--- imgui-sys/project.clj
+++ imgui-sys/project.clj
@@ -1,4 +1,4 @@
-(defproject org.jank-lang.commons/imgui-sys "2026.07-1"
+(defproject org.jank-lang.commons/imgui-sys "2026.08-1"
   :description "Raw package for Dear ImGUI."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MPL 2.0"
