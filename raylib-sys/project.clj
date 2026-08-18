4f09ba24a2531356c6763883923fcfeb3b9c0fd1
Kyle Cesare <kcesare@gmail.com>
2026-08-18 17:43:02 -0600

    Reduce raylib jar file size

--- raylib-sys/project.clj
+++ raylib-sys/project.clj
@@ -6,4 +6,10 @@
   :plugins [[org.jank-lang/lein-jank "2026.07-3"]]
   :middleware [leiningen.jank/middleware]
   :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.06-6"]]
-  :verbatim-paths ["lib/raylib"])
+  :verbatim-paths ["lib/raylib/LICENSE"
+                   "lib/raylib/README.md"
+                   "lib/raylib/raylib.pc.in"
+                   "lib/raylib/CMakeLists.txt"
+                   "lib/raylib/CMakeOptions.txt"
+                   "lib/raylib/cmake"
+                   "lib/raylib/src"])
