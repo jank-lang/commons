eacd19d4655af720e21873bff1b5bf9843aefb4c
jeaye <contact@jeaye.com>
2026-08-18 15:56:35 -0700

    Release ftxui-sys 2026.08-2

--- ftxui-sys/project.clj
+++ ftxui-sys/project.clj
@@ -1,4 +1,4 @@
-(defproject org.jank-lang.commons/ftxui-sys "2026.08-1"
+(defproject org.jank-lang.commons/ftxui-sys "2026.08-2"
   :description "Raw package for FTXUI."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MPL 2.0"
