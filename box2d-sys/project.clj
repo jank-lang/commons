eeef6d9992dce6f06695f67311f9dfd3dd2c07e0
jeaye <contact@jeaye.com>
2026-08-18 15:38:42 -0700

    Release box2d-sys 2026.08-1

--- box2d-sys/project.clj
+++ box2d-sys/project.clj
@@ -1,4 +1,4 @@
-(defproject org.jank-lang.commons/box2d-sys "0.1-SNAPSHOT"
+(defproject org.jank-lang.commons/box2d-sys "2026.08-1"
   :description "Raw package for box2d."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MPL 2.0"
