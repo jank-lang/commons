28057fc226b43e6f8e7350c8b3b7921e6a0daa9c
jeaye <contact@jeaye.com>
2026-08-18 15:39:01 -0700

    Release imgui-opengl2-sys 2026.08-1

--- imgui-opengl2-sys/project.clj
+++ imgui-opengl2-sys/project.clj
@@ -1,10 +1,10 @@
-(defproject org.jank-lang.commons/imgui-opengl2-sys "2026.07-1"
+(defproject org.jank-lang.commons/imgui-opengl2-sys "2026.08-1"
   :description "Raw package for Dear ImGUI OpenGL2 renderer backend."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MPL 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.07-1"]]
-  :dependencies [[org.jank-lang.commons/gl-sys "2026.06-1"]
+  :dependencies [[org.jank-lang.commons/gl-sys "2026.08-1"]
                  [org.jank-lang.commons/imgui-sys "2026.07-1"]]
   :plugins [[org.jank-lang/lein-jank "2026.07-3"]]
   :middleware [leiningen.jank/middleware]
