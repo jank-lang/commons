323d8e47e60fdd6039f32f0df7e57687a18469f4
jeaye <contact@jeaye.com>
2026-08-31 14:22:14 -0700

    Add more examples

---
+++ gl-sys/example/project.clj
@@ -0,0 +1,12 @@
+(defproject gl-sys/example "0.1-SNAPSHOT"
+  :license {:name "MPL 2.0"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :dependencies [[org.jank-lang.commons/gl-sys "2026.08-2"]
+                 [org.jank-lang.commons/glfw-sys "2026.08-1"]]
+  :plugins [[org.jank-lang/lein-jank "2026.08-1"]]
+  :middleware [leiningen.jank/middleware]
+  :main gl-sys.example
+  :profiles {:base {:jank {:target-dir "target/debug"
+                           :optimization-level 0}}
+             :release {:jank {:target-dir "target/release"
+                              :optimization-level 3}}})
---
+++ imgui-glfw-sys/example/project.clj
@@ -0,0 +1,13 @@
+(defproject imgui-glfw-sys/example "0.1-SNAPSHOT"
+  :license {:name "MPL 2.0"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :dependencies [[org.jank-lang.commons/imgui-glfw-sys "2026.08-2"]
+                 [org.jank-lang.commons/imgui-opengl2-sys "2026.08-2"]
+                 [org.jank-lang.commons/gl-sys "2026.08-2"]]
+  :plugins [[org.jank-lang/lein-jank "2026.08-1"]]
+  :middleware [leiningen.jank/middleware]
+  :main imgui-glfw-sys.example
+  :profiles {:base {:jank {:target-dir "target/debug"
+                           :optimization-level 0}}
+             :release {:jank {:target-dir "target/release"
+                              :optimization-level 3}}})
