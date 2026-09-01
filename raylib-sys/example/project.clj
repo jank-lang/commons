c82ff26a41ae0c9e07dbfc330d16dde7e50afe63
jeaye <contact@jeaye.com>
2026-09-01 12:23:45 -0700

    Update lein-jank in all examples

--- raylib-sys/example/project.clj
+++ raylib-sys/example/project.clj
@@ -1,10 +1,10 @@
-(defproject example "0.1-SNAPSHOT"
+(defproject raylib-sys/example "0.1-SNAPSHOT"
   :license {:name "MPL 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[org.jank-lang.commons/raylib-sys "2026.09-1"]]
-  :plugins [[org.jank-lang/lein-jank "2026.08-1"]]
+  :plugins [[org.jank-lang/lein-jank "2026.09-1"]]
   :middleware [leiningen.jank/middleware]
-  :main example.main
+  :main raylib-sys.example
   :profiles {:base {:jank {:target-dir "target/debug"
                            :optimization-level 0}}
              :release {:jank {:target-dir "target/release"
