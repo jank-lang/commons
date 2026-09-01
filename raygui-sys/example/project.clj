1016e0484e186d151471f89fc88c3bdd962ab927
jeaye <contact@jeaye.com>
2026-09-01 16:36:53 -0700

    Release raygui-sys 2026.09-1

--- raygui-sys/example/project.clj
+++ raygui-sys/example/project.clj
@@ -1,7 +1,7 @@
 (defproject raygui-sys/example "0.1-SNAPSHOT"
   :license {:name "MPL 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :dependencies [[org.jank-lang.commons/raygui-sys "0.1-SNAPSHOT"]]
+  :dependencies [[org.jank-lang.commons/raygui-sys "2026.09-1"]]
   :plugins [[org.jank-lang/lein-jank "2026.09-1"]]
   :middleware [leiningen.jank/middleware]
   :main raygui-sys.example
