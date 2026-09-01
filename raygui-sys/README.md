d0559c549b6225c8c57fa5c753b37524649f43e4
jeaye <contact@jeaye.com>
2026-09-01 16:28:52 -0700

    Update example

---
+++ raygui-sys/README.md
@@ -0,0 +1,20 @@
+# raygui-sys
+[![Clojars Project](https://img.shields.io/clojars/v/org.jank-lang.commons/raygui-sys.svg)](https://releases.clojars.org/org.jank-lang.commons/raygui-sys)
+
+Raw package for raygui.
+
+## Usage
+Add this project to your Leiningen `:dependencies` vector:
+
+```clojure
+[org.jank-lang.commons/raygui-sys "2026.09-1"]
+```
+
+## Example
+You can find an example project [here](./example). You're welcome to copy this
+to get started! To run the example project, use Leiningen:
+
+```bash
+cd example
+lein run
+```
--- raygui-sys/project.clj
+++ raygui-sys/project.clj
@@ -3,16 +3,15 @@
   :url "https://github.com/jank-lang/commons"
   :license {:name "MPL 2.0"
             :url  "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :plugins [[org.jank-lang/lein-jank "2026.07-3"]]
+  :plugins [[org.jank-lang/lein-jank "2026.09-1"]]
   :middleware [leiningen.jank/middleware]
   :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.07-1"]]
   ;; raygui at this revision calls raylib's TextToFloat, which raylib gained in
   ;; 5.5. raygui carries its own copy, but only behind RAYGUI_STANDALONE, which
   ;; this package does not define. So raylib-sys has to stay on raylib 5.5 or
   ;; newer, and against 5.0 that call is an undefined symbol.
-  :dependencies [[org.jank-lang.commons/raylib-sys "2026.08-2"]]
+  :dependencies [[org.jank-lang.commons/raylib-sys "2026.09-1"]]
   :verbatim-paths ["lib/raygui/LICENSE"
-                   "lib/raygui/README.md"
-                   "lib/raygui/src"
+                   "lib/raygui/src/raygui.h"
                    "CMakeLists.txt"
                    "raygui_impl.c"])
