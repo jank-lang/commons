710d5297c016a4a7a41350a761c096f01594c70e
jeaye <contact@jeaye.com>
2026-09-02 12:00:54 -0700

    Release box2d-sys 2026.09-2

--- box2d-sys/project.clj
+++ box2d-sys/project.clj
@@ -1,9 +1,9 @@
-(defproject org.jank-lang.commons/box2d-sys "2026.09-1"
+(defproject org.jank-lang.commons/box2d-sys "2026.09-2"
   :description "Raw package for box2d."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MIT"
             :url  "https://github.com/erincatto/box2d/blob/main/LICENSE"}
   :plugins [[org.jank-lang/lein-jank "2026.09-4"]]
   :middleware [leiningen.jank/middleware]
-  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "testing-SNAPSHOT"]]
+  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.09-1"]]
   :verbatim-paths ["lib/box2d"])
