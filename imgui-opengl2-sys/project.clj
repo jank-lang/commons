12ecc9ee0ed00b530e81a64474a89ecfdc6559b8
jeaye <contact@jeaye.com>
2026-09-02 12:01:01 -0700

    Release imgui-opengl2-sys 2026.09-2

--- imgui-opengl2-sys/project.clj
+++ imgui-opengl2-sys/project.clj
@@ -1,11 +1,11 @@
-(defproject org.jank-lang.commons/imgui-opengl2-sys "2026.09-1"
+(defproject org.jank-lang.commons/imgui-opengl2-sys "2026.09-2"
   :description "Raw package for Dear ImGUI OpenGL2 renderer backend."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MIT"
             :url "https://github.com/ocornut/imgui/blob/master/LICENSE.txt"}
-  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "testing-SNAPSHOT"]]
+  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.09-1"]]
   :dependencies [[org.jank-lang.commons/gl-sys "2026.09-1"]
-                 [org.jank-lang.commons/imgui-sys "2026.08-2"]]
+                 [org.jank-lang.commons/imgui-sys "2026.09-2"]]
   :plugins [[org.jank-lang/lein-jank "2026.09-4"]]
   :middleware [leiningen.jank/middleware]
   :verbatim-paths ["CMakeLists.txt"])
