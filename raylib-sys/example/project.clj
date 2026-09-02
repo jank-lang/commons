f719239bc69d0d9901ff9f2c6882262c6ce014af
jeaye <contact@jeaye.com>
2026-09-01 23:29:47 -0700

    CI shit

--- raylib-sys/example/project.clj
+++ raylib-sys/example/project.clj
@@ -2,7 +2,7 @@
   :license {:name "MPL 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[org.jank-lang.commons/raylib-sys "2026.09-1"]]
-  :plugins [[org.jank-lang/lein-jank "2026.09-3"]]
+  :plugins [[org.jank-lang/lein-jank "testing-SNAPSHOT"]]
   :middleware [leiningen.jank/middleware]
   :main raylib-sys.example
   :profiles {:base {:jank {:target-dir "target/debug"
