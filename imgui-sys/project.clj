b61ee9e4f69b2d519625f6b0512893fc35a8328c
jeaye <contact@jeaye.com>
2026-09-02 12:00:57 -0700

    Release imgui-sys 2026.09-2

--- imgui-sys/project.clj
+++ imgui-sys/project.clj
@@ -1,9 +1,9 @@
-(defproject org.jank-lang.commons/imgui-sys "2026.09-1"
+(defproject org.jank-lang.commons/imgui-sys "2026.09-2"
   :description "Raw package for Dear ImGUI."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MIT"
             :url "https://github.com/ocornut/imgui/blob/master/LICENSE.txt"}
-  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "testing-SNAPSHOT"]]
+  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.09-1"]]
   :dependencies []
   :plugins [[org.jank-lang/lein-jank "2026.09-4"]]
   :middleware [leiningen.jank/middleware]
