5d7f2b285a14db721c1b1b0b2b69c042d6c1e486
jeaye <contact@jeaye.com>
2026-09-01 18:44:22 -0700

    Bump lein-jank again

--- box3d-sys/example/project.clj
+++ box3d-sys/example/project.clj
@@ -2,7 +2,7 @@
   :license {:name "MPL 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[org.jank-lang.commons/box3d-sys "2026.09-1"]]
-  :plugins [[org.jank-lang/lein-jank "2026.09-2"]]
+  :plugins [[org.jank-lang/lein-jank "2026.09-3"]]
   :middleware [leiningen.jank/middleware]
   :main box3d-sys.example
   :profiles {:base {:jank {:target-dir "target/debug"
--- glfw-sys/example/project.clj
+++ glfw-sys/example/project.clj
@@ -3,7 +3,7 @@
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[org.jank-lang.commons/gl-sys "2026.09-1"]
                  [org.jank-lang.commons/glfw-sys "2026.09-1"]]
-  :plugins [[org.jank-lang/lein-jank "2026.09-2"]]
+  :plugins [[org.jank-lang/lein-jank "2026.09-3"]]
   :middleware [leiningen.jank/middleware]
   :main glfw-sys.example
   :profiles {:base {:jank {:target-dir "target/debug"
