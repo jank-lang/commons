f2b333c32b2d45a8fb833816686a5036313e9ae3
jeaye <contact@jeaye.com>
2026-09-03 13:02:10 -0700

    Release box3d-sys 2026.09-3

--- box3d-sys/project.clj
+++ box3d-sys/project.clj
@@ -1,9 +1,9 @@
-(defproject org.jank-lang.commons/box3d-sys "2026.09-2"
+(defproject org.jank-lang.commons/box3d-sys "2026.09-3"
   :description "Raw package for box3d."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MIT"
             :url  "https://github.com/erincatto/box3d/blob/main/LICENSE"}
   :plugins [[org.jank-lang/lein-jank "2026.09-5"]]
   :middleware [leiningen.jank/middleware]
-  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.09-1"]]
+  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.09-2"]]
   :verbatim-paths ["lib/box3d"])
