1a0fbd4727c764b74f8afef9f1f264ee427057a2
jeaye <contact@jeaye.com>
2026-09-03 11:36:26 -0700

    Release glfw-sys 2026.09-2

--- glfw-sys/project.clj
+++ glfw-sys/project.clj
@@ -1,8 +1,8 @@
-(defproject org.jank-lang.commons/glfw-sys "2026.09-1"
+(defproject org.jank-lang.commons/glfw-sys "2026.09-2"
   :description "Raw package for glfw3."
   :url "https://github.com/jank-lang/commons"
   :license {:name "zlib/libpng"
             :url "https://github.com/glfw/glfw/blob/master/LICENSE.md"}
   :plugins [[org.jank-lang/lein-jank "2026.09-4"]]
   :middleware [leiningen.jank/middleware]
-  :build-dependencies [[org.jank-lang.commons/jank-build-pkg-config "2026.06-1"]])
+  :build-dependencies [[org.jank-lang.commons/jank-build-pkg-config "2026.09-1"]])
