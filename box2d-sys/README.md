9502003a6717f1dcab30ad8e23988fa2114e7320
jeaye <contact@jeaye.com>
2026-09-03 13:01:54 -0700

    Release box2d-sys 2026.09-3

--- box2d-sys/README.md
+++ box2d-sys/README.md
@@ -7,7 +7,7 @@ Raw package for box2d.
 Add this project to your Leiningen `:dependencies` vector:
 
 ```clojure
-[org.jank-lang.commons/box2d-sys "2026.09-2"]
+[org.jank-lang.commons/box2d-sys "2026.09-3"]
 ```
 
 ## Example
--- box2d-sys/project.clj
+++ box2d-sys/project.clj
@@ -1,9 +1,9 @@
-(defproject org.jank-lang.commons/box2d-sys "2026.09-2"
+(defproject org.jank-lang.commons/box2d-sys "2026.09-3"
   :description "Raw package for box2d."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MIT"
             :url  "https://github.com/erincatto/box2d/blob/main/LICENSE"}
   :plugins [[org.jank-lang/lein-jank "2026.09-5"]]
   :middleware [leiningen.jank/middleware]
-  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.09-1"]]
+  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.09-2"]]
   :verbatim-paths ["lib/box2d"])
