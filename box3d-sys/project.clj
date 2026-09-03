5d77ffdc17417b9fb4087f0d1003fcddce81e4f0
jeaye <contact@jeaye.com>
2026-09-03 11:59:35 -0700

    Remove lein-jank from the -sys packages

--- box3d-sys/project.clj
+++ box3d-sys/project.clj
@@ -3,7 +3,6 @@
   :url "https://github.com/jank-lang/commons"
   :license {:name "MIT"
             :url  "https://github.com/erincatto/box3d/blob/main/LICENSE"}
-  :plugins [[org.jank-lang/lein-jank "2026.09-5"]]
   :middleware [leiningen.jank/middleware]
   :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.09-1"]]
   :verbatim-paths ["lib/box3d"])
--- raygui-sys/project.clj
+++ raygui-sys/project.clj
@@ -3,7 +3,6 @@
   :url "https://github.com/jank-lang/commons"
   :license {:name "MPL 2.0"
             :url  "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :plugins [[org.jank-lang/lein-jank "2026.09-5"]]
   :middleware [leiningen.jank/middleware]
   :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.09-1"]]
   ;; raygui at this revision calls raylib's TextToFloat, which raylib gained in
