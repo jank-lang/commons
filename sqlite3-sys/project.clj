9cc5080025ae5370d4177231ba0092de23a88115
jeaye <contact@jeaye.com>
2026-09-03 11:36:45 -0700

    Release sqlite3-sys 2026.09-2

--- sqlite3-sys/project.clj
+++ sqlite3-sys/project.clj
@@ -1,8 +1,8 @@
-(defproject org.jank-lang.commons/sqlite3-sys "2026.09-1"
+(defproject org.jank-lang.commons/sqlite3-sys "2026.09-2"
   :description "Raw package for sqlite3."
   :url "https://github.com/jank-lang/commons"
   :license {:name "Public Domain"
             :url "https://www.sqlite.org/copyright.html"}
   :plugins [[org.jank-lang/lein-jank "2026.09-4"]]
   :middleware [leiningen.jank/middleware]
-  :build-dependencies [[org.jank-lang.commons/jank-build-pkg-config "2026.06-1"]])
+  :build-dependencies [[org.jank-lang.commons/jank-build-pkg-config "2026.09-1"]])
