8e65505541aa0f0d8e7458d3b198782322813695
jeaye <contact@jeaye.com>
2026-09-03 13:01:51 -0700

    Release imgui-glfw-sys 2026.09-6

--- imgui-glfw-sys/project.clj
+++ imgui-glfw-sys/project.clj
@@ -1,9 +1,9 @@
-(defproject org.jank-lang.commons/imgui-glfw-sys "2026.09-5"
+(defproject org.jank-lang.commons/imgui-glfw-sys "2026.09-6"
   :description "Raw package for Dear ImGUI GLFW platform backend."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MIT"
             :url "https://github.com/ocornut/imgui/blob/master/LICENSE.txt"}
-  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.09-1"]]
+  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.09-2"]]
   :dependencies [[org.jank-lang.commons/glfw-sys "2026.09-2"]
                  [org.jank-lang.commons/imgui-sys "2026.08-2"]]
   :plugins [[org.jank-lang/lein-jank "2026.09-5"]]
