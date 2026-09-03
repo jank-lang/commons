e87351e7b8c4c3139ce2adb24ce9539170d29fc1
jeaye <contact@jeaye.com>
2026-09-03 13:02:23 -0700

    Release ftxui-sys 2026.09-3

--- ftxui-sys/project.clj
+++ ftxui-sys/project.clj
@@ -1,9 +1,9 @@
-(defproject org.jank-lang.commons/ftxui-sys "2026.09-2"
+(defproject org.jank-lang.commons/ftxui-sys "2026.09-3"
   :description "Raw package for FTXUI."
   :url "https://github.com/jank-lang/commons"
   :license {:name "MIT"
             :url  "https://github.com/ArthurSonzogni/FTXUI/blob/main/LICENSE"}
-  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.09-1"]]
+  :build-dependencies [[org.jank-lang.commons/jank-build-cmake "2026.09-2"]]
   :plugins [[org.jank-lang/lein-jank "2026.09-5"]]
   :middleware [leiningen.jank/middleware]
   :verbatim-paths ["lib/ftxui"])
