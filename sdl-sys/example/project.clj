18e2ae899051dfb902401b5e6920694967e5fcd5
Kyle Cesare <kcesare@gmail.com>
2026-09-04 14:35:36 -0600

    Add sdl-sys

---
+++ sdl-sys/example/project.clj
@@ -0,0 +1,11 @@
+(defproject sdl-sys/example "0.1-SNAPSHOT"
+  :license {:name "MPL 2.0"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :dependencies [[org.jank-lang.commons/sdl-sys "0.1-SNAPSHOT"]]
+  :plugins [[org.jank-lang/lein-jank "2026.09-6"]]
+  :middleware [leiningen.jank/middleware]
+  :main sdl-sys.example
+  :profiles {:base {:jank {:target-dir "target/debug"
+                           :optimization-level 0}}
+             :release {:jank {:target-dir "target/release"
+                              :optimization-level 3}}})
